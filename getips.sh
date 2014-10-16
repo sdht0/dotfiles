352d44fbad4a7f7c3c608c8885134d531452fbfa
Siddhartha <git@siddharthasahu.in>
2014-10-16 18:30:38 +0530

    Add bash alias and tmux status for ip address

---
+++ getips.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+printf "|";
+
+for i in $(ip addr | grep "^[0-9]*:" | cut -d: -f2 | grep -v lo | sed "s/ //");do
+    x=$(ip addr show $i | grep "inet " | grep -v "127.0.0.1" | cut -d/ -f1 | awk "{ print \$2}");printf " $x ($i) |" ;
+done
