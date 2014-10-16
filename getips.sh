5e3b995a9a49cfe53a23b922204c6ef050b0da75
Siddhartha <dev@sdht.in>
2014-10-16 18:35:51 +0530

    Do not display not connected interfaces

--- getips.sh
+++ getips.sh
@@ -3,5 +3,5 @@
 printf "|";
 
 for i in $(ip addr | grep "^[0-9]*:" | cut -d: -f2 | grep -v lo | sed "s/ //");do
-    x=$(ip addr show $i | grep "inet " | grep -v "127.0.0.1" | cut -d/ -f1 | awk "{ print \$2}");printf " $x ($i) |" ;
+    x=$(ip addr show $i | grep "inet " | grep -v "127.0.0.1" | cut -d/ -f1 | awk "{ print \$2}");[[ -n "$x" ]] && printf " $x ($i) |" ;
 done
