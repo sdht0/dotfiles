38d61e0376fd9be96743095d0f520a472f008b4a
Siddhartha <dev@sdht.in>
2014-10-16 23:59:21 +0530

    Bash aliases and tmux fixes

--- getips.sh
+++ getips.sh
@@ -5,5 +5,3 @@ printf "|";
 for i in $(ip addr | grep "^[0-9]*:" | cut -d: -f2 | grep -v lo | sed "s/ //");do
     x=$(ip addr show $i | grep "inet " | grep -v "127.0.0.1" | cut -d/ -f1 | awk "{ print \$2}");[[ -n "$x" ]] && printf " $x ($i) |" ;
 done
-
-# One line: ip addr | grep -B2 "inet " | grep -v link | grep -v "\-\-" | grep -v "lo:" | grep -v "127.0.0.1" | awk "{key=\$0; getline; print key \$0;}" | sed "s/[0-9]\: \(.*\)\:.*inet \(.*\)\/.*/\2 (\1)/"
