e0b22bb64a24d63358f2c3eb3a29e499bcf92742
Siddhartha <dev@sdht.in>
2014-10-20 16:00:33 +0530

    Bash aliases and functions improvements

--- getips.sh
+++ getips.sh
@@ -4,6 +4,6 @@ PATH="/sbin:/usr/sbin::$PATH"
 
 printf "|";
 
-for i in $(ip addr | grep "^[0-9]*:" | cut -d: -f2 | grep -v lo | sed "s/ //");do
+for i in $(ip addr | grep "^[0-9]*:" | cut -d: -f2 | grep -v lo | sed "s/ //" | sort);do
     x=$(ip addr show $i | grep "inet " | grep -v "127.0.0.1" | cut -d/ -f1 | awk "{ print \$2}");[[ -n "$x" ]] && printf " $x ($i) |" ;
 done
