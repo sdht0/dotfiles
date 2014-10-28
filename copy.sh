1d74db271f3e47f42a46f2b13786ef64b544425f
Siddhartha <dev@sdht.in>
2014-10-28 21:12:48 +0530

    Copy script

---
+++ copy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp -v /home/sdh/dotfiles/{.tmux.conf,.bashrc,.gitconfig,.vimrc,.zshrc} /home/sdh/dev/sources/user-files/siddhartha.s/
+cp -v /home/sdh/dotfiles/{getips,getuptime}.sh /home/sdh/dev/sources/user-files/siddhartha.s/dotfiles/
