a78173bd1e48b68bbef88e4ccf1408fbcec42839
Siddhartha <dev@sdht.in>
2014-12-01 15:47:58 +0530

    Small sort ips fix

--- getips.sh
+++ getips.sh
@@ -2,5 +2,5 @@
 
 PATH="/sbin:/usr/sbin::$PATH"
 
-ip -o -f inet addr | grep -v '127.0.0.1' | cut -d'/' -f1 | cut -d" " -f2- | sort | uniq | awk '{print "| "$3" ("$1")"}' | tr '\n' ' '
+ip -o -f inet addr | grep -v '127.0.0.1' | cut -d'/' -f1 | sed -r "s/[ \t]+/ /g" | cut -d" " -f2-4 | sort | uniq | awk '{print "| "$3" ("$1")"}' | tr '\n' ' '
 printf "|"
