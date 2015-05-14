7dfb1bb408ac4a503bad6ddeb9fe9c00ff6cdcd2
Siddhartha <git@siddharthasahu.in>
2015-05-14 12:43:45 +0530

    Update paths

--- copy.sh
+++ copy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cp -rv /home/sdh/dotfiles/{.tmux.conf,.bashrc,.gitconfig,.vim*,.zshrc} /home/sdh/directi/sources/user-files/siddhartha.s/
-cp -rv /home/sdh/dotfiles/{getips,getuptime}.sh /home/sdh/directi/sources/user-files/siddhartha.s/dotfiles/
+cp -rv /home/sdh/dotfiles/scripts/{getips,getuptime}.sh /home/sdh/directi/sources/user-files/siddhartha.s/dotfiles/
