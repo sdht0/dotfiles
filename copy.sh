522943ff3ee466e30f5ff54c0988a1075ca2fe72
Siddhartha <git@siddharthasahu.in>
2015-06-12 11:26:15 +0530

    Copy script updates

--- copy.sh
+++ copy.sh
@@ -2,3 +2,6 @@
 
 cp -rv /home/sdh/dotfiles/{.tmux.conf,.bashrc,.gitconfig,.vim*,.zshrc} /home/sdh/directi/sources/user-files/siddhartha.s/
 cp -rv /home/sdh/dotfiles/scripts/{getips,getuptime}.sh /home/sdh/directi/sources/user-files/siddhartha.s/dotfiles/
+
+cp -rv /home/sdh/dotfiles/{.tmux.conf,.bashrc,.gitconfig,.zshrc} /home/sdh/directi/sources/docker-puppet/base
+cp -rv /home/sdh/dotfiles/scripts/{getips,getuptime}.sh /home/sdh/directi/sources/docker-puppet/base
