aabae700d9d83d489259f9b5e2bfe7b347a74ffc
Siddhartha <git@siddharthasahu.in>
2015-07-04 21:07:39 +0530

    Copy more files

--- copy.sh
+++ copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp -rv /home/sdh/dotfiles/{.tmux.conf,.bashrc,.gitconfig,.vim*,.zshrc} /home/sdh/directi/sources/user-files/siddhartha.s/
-cp -rv /home/sdh/dotfiles/scripts/{getips,getuptime}.sh /home/sdh/directi/sources/user-files/siddhartha.s/dotfiles/
+cp -rv /home/sdh/dotfiles/scripts/{getips,getuptime,copyuserfiles}.sh /home/sdh/directi/sources/user-files/siddhartha.s/dotfiles/
 
 cp -rv /home/sdh/dotfiles/{.tmux.conf,.bashrc,.gitconfig,.zshrc} /home/sdh/directi/sources/docker-puppet/base
 cp -rv /home/sdh/dotfiles/scripts/{getips,getuptime}.sh /home/sdh/directi/sources/docker-puppet/base
