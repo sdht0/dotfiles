5ba6c81d83482c8082909391fa9d95b0d110920c
Siddhartha <git@siddharthasahu.in>
2015-11-12 20:36:51 +0530

    path update

--- copy.sh
+++ copy.sh
@@ -3,7 +3,7 @@
 cp -rv /home/sdh/dotfiles/{.tmux.conf,.bashrc,.gitconfig,.vim*,.zshrc} /home/sdh/directi/sources/user-files/siddhartha.s/
 cp -rv /home/sdh/dotfiles/scripts/{getips,getuptime,copyuserfiles}.sh /home/sdh/directi/sources/user-files/siddhartha.s/dotfiles/
 
-cp -rv /home/sdh/dotfiles/{.tmux.conf,.bashrc,.gitconfig,.zshrc} /home/sdh/directi/sources/docker-puppet/base
-cp -rv /home/sdh/dotfiles/scripts/{getips,getuptime}.sh /home/sdh/directi/sources/docker-puppet/base
-cp -rv /home/sdh/dotfiles/{.tmux.conf,.bashrc,.gitconfig,.zshrc} /home/sdh/directi/sources/docker-puppet/base5
-cp -rv /home/sdh/dotfiles/scripts/{getips,getuptime}.sh /home/sdh/directi/sources/docker-puppet/base5
+cp -rv /home/sdh/dotfiles/{.tmux.conf,.bashrc,.gitconfig,.zshrc} /home/sdh/directi/sources/docker/base
+cp -rv /home/sdh/dotfiles/scripts/{getips,getuptime}.sh /home/sdh/directi/sources/docker/base
+cp -rv /home/sdh/dotfiles/{.tmux.conf,.bashrc,.gitconfig,.zshrc} /home/sdh/directi/sources/docker/base5
+cp -rv /home/sdh/dotfiles/scripts/{getips,getuptime}.sh /home/sdh/directi/sources/docker/base5
