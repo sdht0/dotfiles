e2cecf30a86723c63579803cd9b076056dab205e
Siddhartha <dev@sdht.in>
2020-06-27 08:31:21 -0400

    Change color

--- p10k.zsh
+++ p10k.zsh
@@ -209,7 +209,7 @@
 
   ##################################[ dir: current directory ]##################################
   # Current directory background color.
-  typeset -g POWERLEVEL9K_DIR_BACKGROUND=magenta
+  typeset -g POWERLEVEL9K_DIR_BACKGROUND=14
   # Default current directory foreground color.
   typeset -g POWERLEVEL9K_DIR_FOREGROUND=254
   # If directory is too long, shorten some of its segments to the shortest possible unique
