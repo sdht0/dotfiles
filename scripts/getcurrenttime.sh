b8f03ac0ffa5ff3758e52896aff0b04ac325724b
Siddhartha <dev@sdht.in>
2021-03-05 21:22:50 -0500

    Don't pad with spaces

--- scripts/getcurrenttime.sh
+++ scripts/getcurrenttime.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-time=$(date +'%I:%M%P') && \
+time=$(date +'%-I:%M%P') && \
 echo -n $time | xclip -selection clipboard && \
 notify-send "Time '$time' copied to clipboard." || \
 notify-send "Failed to get time."
