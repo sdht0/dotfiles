72e2b2f4ab4011d0ea2739a1592b6e8a35d23ce0
Siddhartha Sahu <dev@sdht.in>
2025-02-01 20:29:24 -0500

    feat: unify tmux.conf

--- setup.sh
+++ setup.sh
@@ -4,26 +4,31 @@ dotfiles=~/.config/dotfiles
 bkdir=~/.cache/dotfiles-"$(date +"%Y%m%d-%H%M%S")"
 
 link_file() {
-    [[ -z "$1" ]] && return -1
+    [[ -z "${1:-}" ]] && return -1
     dest="${2:-.$1}"
     echo "Setting up $dest"
-    [[ -L ~/"$dest" ]] && echo "Backing up $dest" && mkdir -p ${bkdir} && mv ~/"$dest" ${bkdir}/"$dest"
+    [[ -e ~/"$dest" ]] && echo "Backing up $dest" && mkdir -p ${bkdir} && mv ~/"$dest" ${bkdir}/"$dest"
     ln -s "$dotfiles/$1" ~/$dest
 }
 
 echo "Setting up .bashrc"
-[[ -r ~/.bashrc ]] && echo "Backing up .bashrc" && mkdir -p ${bkdir} && mv ~/.bashrc ${bkdir}/.bashrc
+[[ -e ~/.bashrc ]] && echo "Backing up .bashrc" && mkdir -p ${bkdir} && mv ~/.bashrc ${bkdir}/.bashrc
 echo -e ". $dotfiles/bashrc\n[[ -f $dotfiles.safe/bashrc ]] && . $dotfiles.safe/bashrc || true" > ~/.bashrc
 
 echo "Setting up .zshrc"
-[[ -r ~/.zshrc ]] && echo "Backing up .zshrc" && mkdir -p ${bkdir} && mv ~/.zshrc ${bkdir}/.zshrc
+[[ -e ~/.zshrc ]] && echo "Backing up .zshrc" && mkdir -p ${bkdir} && mv ~/.zshrc ${bkdir}/.zshrc
 echo -e ". ~/.bashrc\n. $dotfiles/zshrc\n[[ -f $dotfiles.safe/zshrc ]] && . $dotfiles.safe/zshrc || true" > ~/.zshrc
+
+if [[ -f "$DOTFILES/profile.server" ]];then
+    link_file "server.tmux.conf"
+else
+    link_file "tmux.conf"
+fi
+link_file "gitconfig"
+link_file "vimrc"
+
+# link_file "xinitrc"
 
 # echo "Setting up .emacs"
 # [[ -r ~/.emacs.d/init.el ]] && echo "Backing up init.el" mkdir -p ${bkdir} && && mv ~/.emacs.d/init.el ${bkdir}/.emacs.d/init.el.bk
 # link_file "emacs.init.el" ".emacs.d/init.el"
-
-link_file "tmux.conf"
-link_file "gitconfig"
-link_file "vimrc"
-# link_file "xinitrc"
